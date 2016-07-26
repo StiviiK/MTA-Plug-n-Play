79c94cbbaa9fc5fd74850ea589df4530dd971390
StiviiK <stefan.kuerzeder@gmail.com>
2016-07-26 11:13:45 +0200

    Added dependency check Added FileSystem Modul

--- server/Main.lua
+++ server/Main.lua
@@ -2,11 +2,18 @@ Main = {}
 
 function Main.resourceStart()
 	-- Instantiate Core
-	core = Core:new()
+    if type(createFilesystemInterface) == "function" then
+	    core = Core:new()
+    else
+        outputDebugString("MTA:Eir FileSystem Module is not present! See: https://wiki.multitheftauto.com/wiki/Modules/FileSystem", 1)
+        stopResource(getThisResource())
+    end
 end
 addEventHandler("onResourceStart", resourceRoot, Main.resourceStart, true, "high+99999")
 
 function Main.resourceStop()
-	delete(core)
+    if core then
+		delete(core)
+    end
 end
 addEventHandler("onResourceStop", resourceRoot, Main.resourceStop, true, "low-99999")
